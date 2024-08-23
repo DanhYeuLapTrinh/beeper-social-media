f1eba7f5c561b7bd3a602dcb403dd8f9f7131236
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2024-08-23 08:45:51 +0700

    feat: :sparkles: pagination (not done)

--- client/src/hooks/problems/use-problems.ts
+++ client/src/hooks/problems/use-problems.ts
@@ -1,8 +1,9 @@
+import { GetProblemsFilter } from '@/models/problem.model'
 import { getProblems } from '@/services/problem.services'
 import { useMutation } from '@tanstack/react-query'
 
 export const useProblems = () => {
   return useMutation({
-    mutationFn: () => getProblems()
+    mutationFn: (variables?: GetProblemsFilter) => getProblems(variables)
   })
 }
